f46e62b7cdc9a4116f9528c42e98d8d18d3020be
Kavya Sahithi <kavya.lily@gmail.com>
2021-01-19 16:36:43 +0100

    added on click colors

--- src/components/square.js
+++ src/components/square.js
@@ -2,8 +2,24 @@ import React from 'react'
 
 function Square(props) {
   const { onClick, value } = props
+
+  const giveMeColor = () => {
+    switch (value) {
+      case 'X':
+        return '#D6E075'
+      case 'O':
+        return 'pink'
+      default:
+        return 'lightblue'
+    }
+  }
+
   return (
-    <button onClick={onClick} className='square'>
+    <button
+      onClick={onClick}
+      style={{ background: giveMeColor() }}
+      className='square'
+    >
       {value}
     </button>
   )
